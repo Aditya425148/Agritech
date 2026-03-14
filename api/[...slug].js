d30cccf827382cb48ad324f64484a8f422a40573
Aditya425148 <aadi100right@gmail.com>
2026-03-14 20:16:39 +0530

    Delete api directory

--- api/[...slug].js
+++
@@ -1,5 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../backend/server");
-
-// This catch-all API endpoint lets Vercel route any /api/* request to the Express app.
-module.exports = serverless(app);
